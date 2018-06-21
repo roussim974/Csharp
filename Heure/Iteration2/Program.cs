b2a78456f6a53ae722eff42456442715f3c36535
[roussim974] <35833049+roussim974@users.noreply.github.com>
2018-06-21 10:29:15 +0200

    premier

---
+++ Heure/Iteration2/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+//using System.Collections.Generic;
+using System.IO;
+//using System.Linq;
+using System.Net;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Web;
+//using Newtonsoft.Json;
+
+namespace Iteration2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a request for the URL.   
+            WebRequest request = WebRequest.Create("http://data.metromobilite.fr/api/linesNear/json?x=5.704708&y=45.205311&dist=600&details=false");
+            
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+            //Console.WriteLine(responseFromServer);
+
+            List <json2csharp> donnees = JsonConvert.DeserializeObject<List<json2csharp>>(responseFromServer);
+
+
+            foreach (json2csharp donnee in donnees)
+            {
+                Console.WriteLine(donnee.id);
+                Console.WriteLine(donnee.name);
+                Console.WriteLine(donnee.lon);
+                Console.WriteLine(donnee.lat); 
+                foreach (string line in donnee.lines)
+                {
+                    Console.WriteLine(line);
+                }
+                //Console.WriteLine(donnee.lines);
+            }
+
+            // Clean up the streams and the response. 
+            Console.Read();
+            reader.Close();
+            response.Close();
+        }
+    }
+}
---
+++ Heure/Iteration2/json2csharp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration2
+{
+    class json2csharp
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double lon { get; set; }
+        public double lat { get; set; }
+        public List<string> lines { get; set; }
+    }
+}
